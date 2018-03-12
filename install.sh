dc175a2e92584bce0dc41f6c69d558841ebf1fa3
matthieu <matthieu@treussart.com>
2018-03-12 06:09:40 +0100

    update bro 2.5.2 to 2.5.3

--- install.sh
+++ install.sh
@@ -30,19 +30,19 @@ fi
 # Debian
 if [ -f /etc/debian_version ]; then
     if ! type bro ; then
-        apt-get install curl ca-certificates build-essential tcpdump cmake make gcc g++ flex bison libpcap-dev  python-dev swig zlib1g-dev screen tshark apache2 libssl1.0-dev libgeoip-dev wget git
-        wget -P /opt https://www.bro.org/downloads/bro-2.5.2.tar.gz
-        tar xvf /opt/bro-2.5.2.tar.gz -C /opt
-        ( cd /opt/bro-2.5.2 && ./configure )
-        ( cd /opt/bro-2.5.2 && make )
-        ( cd /opt/bro-2.5.2 && make install )
+        apt-get install -y curl ca-certificates build-essential tcpdump cmake make gcc g++ flex bison libpcap-dev  python-dev swig zlib1g-dev screen tshark apache2 libssl1.0-dev libgeoip-dev wget git
+        wget -P /opt https://www.bro.org/downloads/bro-2.5.3.tar.gz
+        tar xvf /opt/bro-2.5.3.tar.gz -C /opt
+        ( cd /opt/bro-2.5.3 && ./configure )
+        ( cd /opt/bro-2.5.3 && make )
+        ( cd /opt/bro-2.5.3 && make install )
         export PATH=/usr/local/bro/bin:$PATH
         export LD_LIBRARY_PATH=/usr/local/bro/lib/
         ( cd /usr/local/bin && ln -s /usr/local/bro/bin/bro bro )
         ( cd /usr/local/bin && ln -s /usr/local/bro/bin/broctl broctl )
         # Activer af_packet :
         git clone https://github.com/J-Gras/bro-af_packet-plugin.git /opt/bro-af_packet-plugin/
-        ( cd /opt/bro-af_packet-plugin/ && ./configure --bro-dist=/opt/bro-2.5.2 )
+        ( cd /opt/bro-af_packet-plugin/ && ./configure --bro-dist=/opt/bro-2.5.3 )
         ( cd /opt/bro-af_packet-plugin/ && make )
         ( cd /opt/bro-af_packet-plugin/ && make install )
     fi
