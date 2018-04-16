66ac242e4dfc9af27a3bcd249e148a7e618b247f
matthieu <matthieu@treussart.com>
2018-04-16 06:36:29 +0200

    Update install

--- install.sh
+++ install.sh
@@ -21,22 +21,39 @@ if [[ $OSTYPE == *"darwin"* ]]; then
         rules="/usr/local/opt/bro/share/bro/"
     fi
 fi
-# Debian
 if [ -f /etc/debian_version ]; then
     if ! type bro ; then
-        sudo apt update
-        sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl1.0-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
-        wget https://www.bro.org/downloads/bro-"$BRO_VERSION".tar.gz
-        tar xf bro-"$BRO_VERSION".tar.gz
-        ( cd bro-"$BRO_VERSION" && ./configure )
-        ( cd bro-"$BRO_VERSION" && make -j$(nproc)  )
-        ( cd bro-"$BRO_VERSION" && sudo make install )
-        rm bro-"$BRO_VERSION".tar.gz && rm -rf bro-"$BRO_VERSION"
-        export PATH=/usr/local/bro/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/bro/lib/:$LD_LIBRARY_PATH
-        sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
-        sudo chown $(whoami) $( which bro )
-        sudo chown -R $(whoami) /usr/local/bro/etc/
-        sudo chown -R $(whoami) /etc/bro/site/
+        cat /etc/issue
+        # Ubuntu
+        if [[ $( cat /etc/issue ) == *"Ubuntu"* ]]; then
+            sudo apt update
+            sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
+            wget https://www.bro.org/downloads/bro-"$BRO_VERSION".tar.gz
+            tar xf bro-"$BRO_VERSION".tar.gz
+            ( cd bro-"$BRO_VERSION" && ./configure )
+            ( cd bro-"$BRO_VERSION" && make -j$(nproc)  )
+            ( cd bro-"$BRO_VERSION" && sudo make install )
+            rm bro-"$BRO_VERSION".tar.gz && rm -rf bro-"$BRO_VERSION"
+            export PATH=/usr/local/bro/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/bro/lib/:$LD_LIBRARY_PATH
+            sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
+            sudo chown $(whoami) $( which bro )
+            sudo chown -R $(whoami) /usr/local/bro/etc/
+            sudo chown -R $(whoami) /etc/bro/site/
+        else # Debian
+            sudo apt update
+            sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl1.0-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
+            wget https://www.bro.org/downloads/bro-"$BRO_VERSION".tar.gz
+            tar xf bro-"$BRO_VERSION".tar.gz
+            ( cd bro-"$BRO_VERSION" && ./configure )
+            ( cd bro-"$BRO_VERSION" && make -j$(nproc)  )
+            ( cd bro-"$BRO_VERSION" && sudo make install )
+            rm bro-"$BRO_VERSION".tar.gz && rm -rf bro-"$BRO_VERSION"
+            export PATH=/usr/local/bro/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/bro/lib/:$LD_LIBRARY_PATH
+            sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
+            sudo chown $(whoami) $( which bro )
+            sudo chown -R $(whoami) /usr/local/bro/etc/
+            sudo chown -R $(whoami) /etc/bro/site/
+        fi
     fi
     config="/usr/local/bro/etc/"
     rules="/etc/bro/site/"
