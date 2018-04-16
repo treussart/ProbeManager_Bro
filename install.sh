f48c28be8f9dc697cd2922ea76b414833b316a2c
matthieu <matthieu@treussart.com>
2018-04-16 22:50:59 +0200

    Update install

--- install.sh
+++ install.sh
@@ -29,16 +29,14 @@ if [ -f /etc/debian_version ]; then
             echo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /" | sudo tee -a /etc/apt/sources.list
             sudo apt-get update -qq
             sudo apt-get -y --allow-unauthenticated install bro
-            sudo find /usr -name bro
-            sudo find /etc -name bro
-            sudo find / -name bro
-            sudo find / -name local.bro
-            sudo find / -name networks.cfg
-            export PATH=/usr/local/bro/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/bro/lib/:$LD_LIBRARY_PATH
+            export PATH=/opt/bro/bin:$PATH && export LD_LIBRARY_PATH=/opt/bro/lib/:$LD_LIBRARY_PATH
             sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
             sudo chown $(whoami) $( which bro )
             sudo chown -R $(whoami) /usr/local/bro/etc/
             sudo chown -R $(whoami) /etc/bro/site/
+            ls /opt/bro/share/bro
+            config="/opt/bro/etc/"
+            rules="/opt/bro/share/bro/"
         elif [[ $( cat /etc/issue ) == *"Ubuntu"* ]]; then
             sudo apt update
             sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
@@ -53,6 +51,8 @@ if [ -f /etc/debian_version ]; then
             sudo chown $(whoami) $( which bro )
             sudo chown -R $(whoami) /usr/local/bro/etc/
             sudo chown -R $(whoami) /etc/bro/site/
+            config="/usr/local/bro/etc/"
+            rules="/etc/bro/"
         else # Debian
             sudo apt update
             sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl1.0-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
@@ -67,10 +67,10 @@ if [ -f /etc/debian_version ]; then
             sudo chown $(whoami) $( which bro )
             sudo chown -R $(whoami) /usr/local/bro/etc/
             sudo chown -R $(whoami) /etc/bro/site/
+            config="/usr/local/bro/etc/"
+            rules="/etc/bro/"
         fi
     fi
-    config="/usr/local/bro/etc/"
-    rules="/etc/bro/site/"
 fi
 
 if ! type bro ; then
