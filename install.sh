7acfdcbec47e00f988d1192571ae0b3e194e32bd
Treussart Matthieu <matthieu@treussart.com>
2018-04-16 15:37:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,17 +25,9 @@ if [ -f /etc/debian_version ]; then
     if ! type bro ; then
         # Ubuntu
         if [[ $( cat /etc/issue ) == *"Ubuntu"* ]]; then
-            locate python3.5m
-            locate libpython3.5m
-            export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+            sudo sh -c "echo 'deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/bro.list"
             sudo apt update
-            sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
-            wget https://www.bro.org/downloads/bro-"$BRO_VERSION".tar.gz
-            tar xf bro-"$BRO_VERSION".tar.gz
-            ( cd bro-"$BRO_VERSION" && ./configure )
-            ( cd bro-"$BRO_VERSION" && make -j$(nproc)  )
-            ( cd bro-"$BRO_VERSION" && sudo make install )
-            rm bro-"$BRO_VERSION".tar.gz && sudo rm -rf bro-"$BRO_VERSION"
+            sudo apt -y install bro
             export PATH=/usr/local/bro/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/bro/lib/:$LD_LIBRARY_PATH
             sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
             sudo chown $(whoami) $( which bro )
