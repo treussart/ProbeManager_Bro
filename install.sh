60fbbcca0778c2835676a381348f506ef9106567
Treussart Matthieu <matthieu@treussart.com>
2018-04-27 14:10:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,23 +21,25 @@ if [[ $OSTYPE == *"darwin"* ]]; then
         rules="/usr/local/opt/bro/share/bro/"
     fi
 fi
-if [ -f /etc/debian_version ]; then
-    if ! type bro ; then
-        # Ubuntu
-        if [[ "$TRAVIS" = true ]]; then
+if [ -f /etc/debian_version ]; then   
+    # Ubuntu
+    if [[ "$TRAVIS" = true ]]; then
+        if ! type bro ; then
             curl http://download.opensuse.org/repositories/network:bro/xUbuntu_14.04/Release.key | sudo apt-key add -
             echo "deb http://download.opensuse.org/repositories/network:/bro/xUbuntu_14.04/ /" | sudo tee -a /etc/apt/sources.list
             sudo apt-get update -qq
             sudo apt-get -y --allow-unauthenticated install bro
             export PATH=/opt/bro/bin:$PATH && export LD_LIBRARY_PATH=/opt/bro/lib/:$LD_LIBRARY_PATH
             sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
-            sudo chown "$SERVER_USER":"$CURRENT_USER" $( which bro )
-            sudo chown -R "$SERVER_USER":"$CURRENT_USER" /opt/bro
-            sudo chmod -R 750 /opt/bro
-            sudo chmod 750 $( which bro )
-            config="/opt/bro/etc/"
-            rules="/opt/bro/share/bro/"
-        else # Debian and ubuntu
+        fi
+        sudo chown "$SERVER_USER":"$CURRENT_USER" $( which bro )
+        sudo chown -R "$SERVER_USER":"$CURRENT_USER" /opt/bro
+        sudo chmod -R 750 /opt/bro
+        sudo chmod 750 $( which bro )
+        config="/opt/bro/etc/"
+        rules="/opt/bro/share/bro/"
+    else # Debian and ubuntu
+        if ! type bro ; then
             sudo apt update
             sudo apt install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev libmagic-dev libgeoip-dev sendmail libcap2-bin wget curl ca-certificates
             wget https://www.bro.org/downloads/bro-"$BRO_VERSION".tar.gz
@@ -48,21 +50,21 @@ if [ -f /etc/debian_version ]; then
             rm bro-"$BRO_VERSION".tar.gz && sudo rm -rf bro-"$BRO_VERSION"
             export PATH=/usr/local/bro/bin:$PATH && export LD_LIBRARY_PATH=/usr/local/bro/lib/:$LD_LIBRARY_PATH
             sudo setcap cap_net_raw,cap_net_admin=eip $( which bro )
-            if [[ "$arg" = 'prod' ]]; then
+        fi
+        if [[ "$arg" = 'prod' ]]; then
                 sudo chown "$SERVER_USER":"$CURRENT_USER" $( which bro )
                 sudo chown -R "$SERVER_USER":"$CURRENT_USER" /usr/local/bro
                 sudo chown -R "$SERVER_USER":"$CURRENT_USER" /etc/bro
                 sudo chmod -R 750 /etc/bro
                 sudo chmod -R 750 /usr/local/bro
                 sudo chmod 750 $( which bro )
-            else
+        else
                 sudo chown "$CURRENT_USER" $( which bro )
                 sudo chown -R "$CURRENT_USER" /usr/local/bro
                 sudo chown -R "$CURRENT_USER" /etc/bro
-            fi
-            config="/usr/local/bro/etc/"
-            rules="/etc/bro/"
         fi
+        config="/usr/local/bro/etc/"
+        rules="/etc/bro/"
     fi
 fi
 
