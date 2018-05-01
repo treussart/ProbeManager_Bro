6dd9d6e6bb5d8209be3d08c615a400a016d04e0f
matthieu <matthieu@treussart.com>
2018-05-01 17:08:54 +0200

    Refactoring Delete and Save in models

--- api/views.py
+++ api/views.py
@@ -5,8 +5,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from bro.api.serializers import ConfigurationSerializer, BroSerializer, SignatureBroSerializer, ScriptBroSerializer, \
-    RuleSetBroSerializer, IntelSerializer, CriticalStackSerializer, BroUpdateSerializer
+from bro.api import serializers
 from bro.models import Configuration, Bro, SignatureBro, ScriptBro, RuleSetBro, Intel, CriticalStack
 
 
@@ -15,34 +14,51 @@ logger = logging.getLogger(__name__)
 
 class ConfigurationViewSet(viewsets.ModelViewSet):
     queryset = Configuration.objects.all()
-    serializer_class = ConfigurationSerializer
+    serializer_class = serializers.ConfigurationSerializer
 
 
-class BroViewSet(viewsets.ModelViewSet):
+class BroViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin,
+                 mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Bro.objects.all()
-    serializer_class = BroSerializer
+    serializer_class = serializers.BroSerializer
+
+    def update(self, request, pk=None):
+        bro = self.get_object()
+        serializer = serializers.BroUpdateSerializer(bro, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        bro = self.get_object()
+        serializer = serializers.BroUpdateSerializer(bro, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class SignatureBroViewSet(viewsets.ModelViewSet):
     queryset = SignatureBro.objects.all()
-    serializer_class = SignatureBroSerializer
+    serializer_class = serializers.SignatureBroSerializer
 
 
 class ScriptBroViewSet(viewsets.ModelViewSet):
     queryset = ScriptBro.objects.all()
-    serializer_class = ScriptBroSerializer
+    serializer_class = serializers.ScriptBroSerializer
 
 
 class RuleSetBroViewSet(viewsets.ModelViewSet):
     queryset = RuleSetBro.objects.all()
-    serializer_class = RuleSetBroSerializer
+    serializer_class = serializers.RuleSetBroSerializer
 
 
 class IntelViewSet(viewsets.ModelViewSet):
     queryset = Intel.objects.all()
-    serializer_class = IntelSerializer
+    serializer_class = serializers.IntelSerializer
 
 
 class CriticalStackViewSet(viewsets.ModelViewSet):
     queryset = CriticalStack.objects.all()
-    serializer_class = CriticalStackSerializer
+    serializer_class = serializers.CriticalStackSerializer
