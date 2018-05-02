fae2983b78fe959b8906f0a3087de09d17a373c0
matthieu <matthieu@treussart.com>
2018-05-02 06:30:05 +0200

    Update API

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework import mixins
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from bro.api import serializers
 from bro.models import Configuration, Bro, SignatureBro, ScriptBro, RuleSetBro, Intel, CriticalStack
@@ -15,6 +16,12 @@ logger = logging.getLogger(__name__)
 class ConfigurationViewSet(viewsets.ModelViewSet):
     queryset = Configuration.objects.all()
     serializer_class = serializers.ConfigurationSerializer
+
+    @action(detail=True)
+    def test(self, request, pk=None):
+        obj = self.get_object()
+        response = obj.test()
+        return Response(response)
 
 
 class BroViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin,
@@ -38,20 +45,44 @@ class BroViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.Destro
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @action(detail=True)
+    def test_rules(self, request, pk=None):
+        obj = self.get_object()
+        response = obj.test_rules()
+        return Response(response)
+
 
 class SignatureBroViewSet(viewsets.ModelViewSet):
     queryset = SignatureBro.objects.all()
     serializer_class = serializers.SignatureBroSerializer
+
+    @action(detail=True)
+    def test(self, request, pk=None):
+        obj = self.get_object()
+        response = obj.test_all()
+        return Response(response)
 
 
 class ScriptBroViewSet(viewsets.ModelViewSet):
     queryset = ScriptBro.objects.all()
     serializer_class = serializers.ScriptBroSerializer
 
+    @action(detail=True)
+    def test(self, request, pk=None):
+        obj = self.get_object()
+        response = obj.test_all()
+        return Response(response)
+
 
 class RuleSetBroViewSet(viewsets.ModelViewSet):
     queryset = RuleSetBro.objects.all()
     serializer_class = serializers.RuleSetBroSerializer
+
+    @action(detail=True)
+    def test_rules(self, request, pk=None):
+        obj = self.get_object()
+        response = obj.test_rules()
+        return Response(response)
 
 
 class IntelViewSet(viewsets.ModelViewSet):
