78bc4e06d7feca580a493baf72371574095e0d8d
matthieu <matthieu@treussart.com>
2018-05-16 06:54:19 +0200

    Load all the scripts for tests

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import action
 from rest_framework import viewsets, mixins, status
 
 from bro.api import serializers
+from bro.exceptions import TestRuleFailed
 from bro.models import Configuration, Bro, SignatureBro, ScriptBro, RuleSetBro, Intel, CriticalStack
 
 
@@ -122,6 +123,33 @@ class SignatureBroViewSet(viewsets.ModelViewSet):
 class ScriptBroViewSet(viewsets.ModelViewSet):
     queryset = ScriptBro.objects.all()
     serializer_class = serializers.ScriptBroSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        except TestRuleFailed:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        try:
+            partial = kwargs.pop('partial', False)
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=partial)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+
+            if getattr(instance, '_prefetched_objects_cache', None):
+                # If 'prefetch_related' has been applied to a queryset, we need to
+                # forcibly invalidate the prefetch cache on the instance.
+                instance._prefetched_objects_cache = {}
+
+            return Response(serializer.data)
+        except TestRuleFailed:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=True)
     def test(self, request, pk=None):
